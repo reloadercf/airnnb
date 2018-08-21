b5c418e49418b18d2c7b70e0dc4c1e2d1542457b
Andy <web@imandy.ie>
2018-08-21 14:32:42 +0100

    Add semicolons

--- App.js
+++ App.js
@@ -7,10 +7,10 @@
 import React, { Component } from 'react';
 import { StatusBar, AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { Root, configureStore} from './src/navigators/AppNavigator';
 import { NETWORK_INTERFACE } from './src/config';
 
